a913f65d2145fb9b2563ed13f9b8960db7914339
RAbinaya2122 <abinayacse2027@avccengg.net>
2025-10-18 12:38:06 +0530

    login fix

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,93 +1,12 @@
-// import { useEffect, useState } from "react"
-// import axios from 'axios';
-
-// function Home() {
-//   const [newTodo, setNewTodo]= useState("");
-//   const [todos, setTodos] = useState([]);
-//   const email = JSON.parse(localStorage.getItem("email"))
-//   console.log(email)
-//   const addTodo = async (e) => {
-//     e.preventDefault();
-//     if(!newTodo.trim()) return;
-//     try {
-//       const response = await axios.post("https://nm-project-gq9z.onrender.com/todo/create", { title: newTodo, email: email })
-//       setTodos([...todos, response.data])
-//       setNewTodo('');
-//     } catch(error) {
-//       console.log("Error adding todo:",error);
-
-//     }  
-//   };
-
-
-
-//   const fetchTodos = async () => {
-//     try {
-//       const response = await axios.post("https://nm-project-gq9z.onrender.com/todo/get", {email});
-//       setTodos(response.data.response)
-
-//     }catch (error){
-//       console.log("Error fetching todos:",error)
-
-//     }
-//   }
-
-
-
-//   useEffect(() => {
-//     fetchTodos();
-//   }, [todos]);
-//   return (
-//   <div className="min-h-screen bg-gradient-to-br from purple-100 to-purple-700 flex items-center
-//   justify-center p-4">
-//     <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-8">
-      
-//         <h1 className="text-4xl font-bold text-purple-700 mb-8">
-//           Task Manager
-//           </h1>
-      
-//       <form onSubmit={addTodo} className="flex items-center gap-2 shadow-sm border border-gray-200 p-2 rounded-lg">
-//         <input 
-//         className="flex-1 outline-none px-3 py-2 text-gray-700 placeholder-gray-400"
-//         type="text" 
-//         value={newTodo} 
-//         onChange={(e) => setNewTodo(e.target.value)}
-//         placeholder="Enter your task..."
-//         required
-//          />
-//          <button type="submit" className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-md 
-//          font-medium cursor-pointer">Add Task</button>
-//       </form>
-//       <div>
-//         {todos.length === 0 ? (
-//           <div></div>
-//         ) : (
-//           <div>
-//             {todos.map((todo) => (
-//               <div className="bg-purple-400 my-3 rounded-lg px-3 text-white font-bold h-10" key={todo._id}>{todo.title}
-
-//               </div>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   </div>
-//   )
-// }
-
-// export default Home
-
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { PiCornersOutLight } from "react-icons/pi";
 
 function Home() {
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
   const [editId, setEditId] = useState(null);
   const [editValue, setEditValue] = useState("");
-  var email; 
+  const email = localStorage.getItem("email");
 
   // 🔹 Add new todo
   const addTodo = async (e) => {
@@ -156,7 +75,6 @@ function Home() {
   };
 
   useEffect(() => {
-    email =  localStorage.getItem("email");
     fetchTodos();
   }, []);
 
